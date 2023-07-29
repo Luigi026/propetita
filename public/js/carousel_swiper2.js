36938713ba5b64b8e11c9198e5817475896e89c7
Luigi026 <luigi.navarro026@gmail.com>
2023-07-29 20:13:56 -0300

    agregando animaciones, a mejorar

--- public/js/carousel_swiper2.js
+++ public/js/carousel_swiper2.js
@@ -1,6 +1,6 @@
 var swiper = new Swiper(".mySwiper2", {
-    slidesPerView: 3,
-    spaceBetween: 30,
+    slidesPerView: 2,
+    spaceBetween: 10,
     /* pagination: {
       el: ".swiper-pagination",
       clickable: true,
@@ -8,5 +8,19 @@ var swiper = new Swiper(".mySwiper2", {
     autoplay: {
       delay: 2000,
       disableOnInteraction: false,
+    },
+    breakpoints: {
+      480: {
+        slidesPerView: 3,
+        spaceBetween: 30,
+      },
+      768: {
+        slidesPerView: 4,
+        spaceBetween: 40,
+      },
+      1024: {
+        slidesPerView: 5,
+        spaceBetween: 50,
+      }
     },
   });
